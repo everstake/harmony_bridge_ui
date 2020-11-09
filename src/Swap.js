e5ba53cefd7ef63dce722205f2d7b262c0bb446e
svyatoslav.d <svyatoslav.d@blaize.tech>
2020-11-09 12:10:04 +0200

    separate allowance

--- src/Swap.js
+++ src/Swap.js
@@ -7,8 +7,10 @@ const Token = require("./contracts/EdgewareToken");
 
 export function Swap({assetID}) {
     const [balance, setBalance] = useState("");
+    const [allowance, setAllowance] = useState(0);
     const [receiver, setReceiver] = useState("");
     const [assetName, setAssetName] = useState("");
+    const [inputValueAllowance, setInputValueAllowance] = useState(0);
     const [inputValue, setInputValue] = useState(0);
     const {harmony, account} = useContext(AppContext);
 
@@ -21,6 +23,8 @@ export function Swap({assetID}) {
         setBalance(balanceResult.toString());
         const nameResult = await token.methods.name().call();
         setAssetName(nameResult);
+        const allowanceResult = await token.methods.allowance(account, config.bridge).call();
+        setAllowance(Number(String(allowanceResult)));
     };
 
     useEffect(() => {
@@ -31,27 +35,40 @@ export function Swap({assetID}) {
         setReceiver(event.target.value);
     };
 
-    const handleFee = (event) => {
+    const onChangeTransferValue = (event) => {
         setInputValue(event.target.value);
+    };
+
+    const onChangeAllowanceValue = (event) => {
+        setInputValueAllowance(event.target.value);
+    };
+
+    const handleAllowance = async () => {
+        if (!harmony) {
+            return;
+        }
+        const token = await harmony.contracts.createContract(Token.abi, assetID);
+        await token.methods.approve(config.bridge, inputValueAllowance).send({
+            from: account,
+            gasLimit: 8000000,
+            gasPrice: 1000000000
+        });
+
+        refreshInfo().catch();
     };
 
     const handleTransfer = async () => {
         if (!harmony) {
             return;
         }
-        const token = await harmony.contracts.createContract(Token.abi, assetID);
-        await token.methods.approve(config.bridge, inputValue).send({
-            from: account,
-            gas: 80000000,
-            gasPrice: 100000000000
-        });
-
         const bridge = await harmony.contracts.createContract(Bridge.abi, config.bridge);
         await bridge.methods.transferToken(receiver, inputValue, assetID).send({
             from: account,
-            gas: 80000000,
-            gasPrice: 100000000000
+            gasLimit: 8000000,
+            gasPrice: 1000000000
         });
+
+        refreshInfo().catch();
     };
 
     return <div className={"SWapContainer"}>
@@ -66,12 +83,24 @@ export function Swap({assetID}) {
         <span>balance {balance}</span>
         <br/>
 
+        <span className={allowance < inputValue ? "ErrorLabel" : ""}>allowance {allowance}</span>
+        <br/>
+
         <div className={"SwapParams"}>
+            <span>Allowance amount:</span>
+            <input type="text" value={inputValueAllowance} onChange={onChangeAllowanceValue}/>
+
+            <button onClick={handleAllowance}>
+                Approve spend
+            </button>
+
+            <br/>
+
             <span>Receiver:</span>
             <input type="text" value={receiver} onChange={handleReceiver}/>
 
             <span>Amount:</span>
-            <input type="text" value={inputValue} onChange={handleFee}/>
+            <input type="text" value={inputValue} onChange={onChangeTransferValue}/>
 
             <button onClick={handleTransfer}>
                 Transfer
