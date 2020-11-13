260f374ec9fc04fceb04eaa2f166933f3d8d5eb3
svyatoslav.d <svyatoslav.d@blaize.tech>
2020-11-13 17:12:37 +0200

    add edgeware

--- src/Swap.js
+++ src/Swap.js
@@ -10,27 +10,43 @@ export function Swap({assetID}) {
     const [receiver, setReceiver] = useState("one1swpff8afhyyc2yds6z7v2mk8vr5am5gkupgnh4");
     const [assetName, setAssetName] = useState("");
     const [inputValue, setInputValue] = useState(0);
-    const {harmony, account, updateBalance} = useContext(AppContext);
+    const {walletAPI, account} = useContext(AppContext);
+    const [balanceCoin, setBalanceCoin] = useState("");
+
+    const updateCoinBalance = async () => {
+        if (!walletAPI || !walletAPI.blockchain|| !account) {
+            return
+        }
+        const balance = await walletAPI.blockchain.getBalance({
+            address: account,
+            shardID: 0,
+        }).then((r) => r).catch(() => "error");
+        setBalanceCoin(walletAPI.utils.hexToBN(balance.result).toString());
+    };
 
     const refreshInfo = async () => {
-        if (!harmony) {
+        if (!walletAPI || !account || !assetID) {
             return;
         }
+        if (assetID === "Edgeware") {
+            return;
+        }
+
         if (assetID === "Harmony") {
             setAssetName("Harmony");
+            await updateCoinBalance();
         } else {
-            const token = harmony.contracts.createContract(Token.abi, assetID);
+            const token = walletAPI.contracts.createContract(Token.abi, assetID);
             const balanceResult = await token.methods.balanceOf(account).call();
             setBalance(balanceResult.toString());
             const nameResult = await token.methods.name().call();
             setAssetName(nameResult);
         }
-        await updateBalance();
     };
 
     useEffect(() => {
         refreshInfo().catch(console.error);
-    }, [harmony, assetID]);
+    }, [walletAPI, assetID]);
 
     const handleReceiver = (event) => {
         setReceiver(event.target.value);
@@ -41,10 +57,10 @@ export function Swap({assetID}) {
     };
 
     const handleTransferToken = async () => {
-        if (!harmony) {
+        if (!walletAPI) {
             return;
         }
-        const bridge = await harmony.contracts.createContract(Bridge.abi, config.bridge);
+        const bridge = await walletAPI.contracts.createContract(Bridge.abi, config.bridge);
         await bridge.methods.transferToken(receiver, inputValue, assetID).send({
             from: account,
             gasLimit: 8000000,
@@ -55,10 +71,10 @@ export function Swap({assetID}) {
     };
 
     const handleTransferCoin = async () => {
-        if (!harmony) {
+        if (!walletAPI) {
             return;
         }
-        const bridge = await harmony.contracts.createContract(Bridge.abi, config.bridge);
+        const bridge = await walletAPI.contracts.createContract(Bridge.abi, config.bridge);
         await bridge.methods.transferCoin(receiver).send({
             from: account,
             gasLimit: 8000000,
@@ -69,7 +85,10 @@ export function Swap({assetID}) {
         refreshInfo().catch();
     };
 
-    return <div className={"SWapContainer"}>
+    return <div className={"SwapContainer"}>
+        <span>Harmony >>> Edgeware</span>
+        <br/>
+
         <button onClick={refreshInfo}>
             update info
         </button>
@@ -78,7 +97,7 @@ export function Swap({assetID}) {
         <span>Token {assetName}({assetID})</span>
         <br/>
 
-        {assetID === "Harmony" ? "" : <span>token balance {balance}</span>}
+        {assetID === "Harmony" ? <span>token balance {balanceCoin}</span> : <span>token balance {balance}</span>}
         <br/>
 
         <div className={"SwapParams"}>
