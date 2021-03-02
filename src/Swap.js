c1f41490b6b5377c31aba58e50bede4e96ae5b31
spreadzp <spread2009@gmail.com>
2021-03-02 18:37:59 +0200

    fix class error

--- src/Swap.js
+++ src/Swap.js
@@ -1,6 +1,6 @@
-import React, {useContext, useEffect, useState} from "react";
-import {AppContext} from "./App";
-import {Unit} from "@harmony-js/utils/dist/transformers";
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "./App";
+import { Unit } from "@harmony-js/utils/dist/transformers";
 
 import { Input } from 'antd';
 import { ForwardOutlined, ReloadOutlined } from '@ant-design/icons';
@@ -9,27 +9,32 @@ const config = require("./config");
 const Bridge = require("./contracts/Bridge");
 const Token = require("./contracts/EdgewareToken");
 
-export function Swap({assetID}) {
+export function Swap({ assetID }) {
     const [balance, setBalance] = useState("");
-    const [receiver, setReceiver] = useState("one1swpff8afhyyc2yds6z7v2mk8vr5am5gkupgnh4");
+    const [receiver, setReceiver] = useState("5EvoXxzVSBAkRbvDK8U3may1GpYykCdMXwBu9THouFGP1hLH");
     const [assetName, setAssetName] = useState("");
     const [inputValue, setInputValue] = useState(0);
-    const {walletAPI, account} = useContext(AppContext);
+    const { walletAPI, account } = useContext(AppContext);
     const [balanceCoin, setBalanceCoin] = useState("");
 
     const updateCoinBalance = async () => {
         if (!walletAPI || !walletAPI.blockchain || !account) {
             return
         }
-        const balance = await walletAPI.blockchain.getBalance({
-            address: account,
-            shardID: 0,
-        }).then((r) => r).catch(() => "error");
-        setBalanceCoin(walletAPI.utils.hexToBN(balance.result).toString());
+        if (assetName === "Harmony") {
+            const balance = await walletAPI.blockchain.getBalance({
+                address: account,
+                shardID: 0,
+            }).then((r) => r).catch(() => "error");
+
+            setBalanceCoin(walletAPI.utils.hexToBN(balance.result).toString());
+        }
+
     };
 
     const refreshInfo = async () => {
         if (!walletAPI || !account || !assetID) {
+
             return;
         }
         if (assetID === "Edgeware") {
@@ -64,7 +69,6 @@ export function Swap({assetID}) {
         if (!walletAPI) {
             return;
         }
-        console.log('receiver, inputValue, assetID :>> ', receiver, inputValue, assetID, account);
         const bridge = await walletAPI.contracts.createContract(Bridge.abi, config.bridge);
         const res = await bridge.methods.transferToken(receiver, inputValue, assetID).send({
             from: account,
@@ -80,8 +84,6 @@ export function Swap({assetID}) {
         if (!walletAPI) {
             return;
         }
-        console.log('inputValue :>> ', inputValue);
-        console.log('(new Unit(inputValue).asWei()).toWeiString() :>> ',receiver, (new Unit(inputValue).asWei()).toWeiString(), account);
         const bridge = await walletAPI.contracts.createContract(Bridge.abi, config.bridge);
         bridge.methods.transferCoin(receiver).send({
             from: account,
@@ -91,7 +93,7 @@ export function Swap({assetID}) {
         }).then(res => {
             console.log('res1 :>>', res);
         })
-            
+
         refreshInfo().catch();
     };
 
@@ -101,18 +103,18 @@ export function Swap({assetID}) {
         <button onClick={refreshInfo}>
             <ReloadOutlined /> update info
         </button>
-        <br/>
+        <br />
 
         <h5>Token {assetName} ({assetID})</h5>
 
-        {assetID === "Harmony" ? <div class="balance">token balance {balanceCoin}</div> : <div class="balance">token balance {balance}</div>}
-  
+        {assetID === "Harmony" ? <div className="balance">token balance {balanceCoin}</div> : <div className="balance">token balance {balance}</div>}
+
         <div className={"SwapParams"}>
             <Input addonBefore="Receiver:" defaultValue={receiver} onChange={handleReceiver} />
             {/* <input type="text" value={receiver} onChange={handleReceiver}/> */}
 
             {/* <span>Amount:</span> */}
-            <Input addonBefore="Amount:" type="number"  defaultValue={inputValue} onChange={onChangeTransferValue}/>
+            <Input addonBefore="Amount:" type="number" defaultValue={inputValue} onChange={onChangeTransferValue} />
             {/* <input type="number" value={inputValue} onChange={onChangeTransferValue}/> */}
 
             <button onClick={assetID === "Harmony" ? handleTransferCoin : handleTransferToken}>
